1557cb337b8a7a8c4fa0b79c92c2d65b2deacb75
burry <burry439@gmail.com>
2017-11-07 13:28:45 +0200

    second commit

--- models.js
+++ models.js
@@ -1,1 +1,18 @@
-ggg
+var mongoose = require('mongoose')
+
+var eventsSchema = new mongoose.Schema(
+    { 
+        description: String, 
+        startTime: Date,
+        endTime: Date,
+        city: String,
+        country: String,
+        attendees: Number,
+        picture: String,
+        date: Date,
+        title: String
+    });
+
+    var Events = mongoose.model('events', eventsSchema)
+
+    module.exports.Events = Events;
--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 var path = require('path');
+var events = require('./models');
+var mongoose = require('mongoose')
+var bodyParser = require('body-parser')
 const app = express();
+var db = mongoose.connect('mongodb://localhost/travelDB');
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(express.static('./server/static/'));
 app.use(express.static('./client/dist/'));
+
+app.get('./test',(req,res) => {
+  console.log('works')
+  res.send('works')
+})
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, './server/static/index.html'))
