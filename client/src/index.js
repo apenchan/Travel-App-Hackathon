a1928fdca602636a4e7ac28a8c7591ab18f41edf
Lise <nakache.lise@gmail.com>
2017-11-07 12:24:42 +0200

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <br/>
+        Hello world!!
+      </div>
+    );
+  }
+}
+
+export default App;
