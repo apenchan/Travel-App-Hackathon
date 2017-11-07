722a30679380626fc7fed31e5ec6451f7cbe0dbf
daniel <byond21@gmail.com>
2017-11-07 14:30:41 +0200

    adding functions to the eventlistbox and the eventbox

---
+++ EventBox.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+class EventBox extends Component {
+    render() {
+        return (
+            <div className="col-md-3">
+                <div>
+                    <div className="card">
+                        <img className="card-img-top" src="..." alt="Card image cap"/>
+                        <div className="card-body">
+                            <h4 className="card-title"> {this.props.X}</h4>
+                            <p className="card-text">Some quick example text.</p>
+                            <a href="#" className="btn btn-primary">Button</a>
+                            
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default EventBox;
---
+++ EventsListBox.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import EventBox from './EventBox';
+
+class EventsListBox extends React.Component {
+
+    mapTheEvents() {
+        return this.props.PROPSNAME.map(function (event, index) {
+                return <EventBox key={index} {...event}>
+                    {event.name}</EventBox>
+            })
+    }
+
+    render() {
+        return (
+            <div className="row">
+               {this.mapTheEvents()} 
+            </div>
+        );
+    }
+
+}
+
+
+export default EventsListBox;
