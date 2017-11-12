5f86de531c760c3f3b0b0c440963a070257f9977
Lise <nakache.lise@gmail.com>
2017-11-12 15:35:54 +0200

    working on google map

--- client/src/DetailsEvent.js
+++ client/src/DetailsEvent.js
@@ -1,9 +1,7 @@
-// MAKE A ROUTE FROM EVENTBOX TO DETAILSEVENT
-// WHEN CLIK THE BTN "MORE DETAILS", REDIRECTING TO DETAILEVENT
-//PASS THE INDEX THROUGH EVENTLISTBOX-EVENTBOX-DETAILEVENT --> WHEN THE BTN IS CLICKED,REDIRECT TO THE GOOD INDEX
-//APP.GET TO GET THE EVENT/ID
+
 import React from 'react';
 import axios from 'axios'
+import MapContainer from './googleMap';
 
 class DetailsEvent extends React.Component {
 
@@ -15,9 +13,6 @@ class DetailsEvent extends React.Component {
     }
 
     componentWillMount() {
-        // console.log(this.props.event._id)
-        // var eventId = this.props.event._id
-        // console.log("Im in Detail Component")
         axios.get("/event/" + this.props.match.params.eventId)
             .then((response) => {
                 console.log(response.data);
@@ -32,7 +27,6 @@ class DetailsEvent extends React.Component {
 
 
     render() {
-        // console.log(this.props.match.params.eventId)
         return (
             <div className="details">
 
@@ -40,17 +34,21 @@ class DetailsEvent extends React.Component {
 
                 <div className="card-body-details">
                     <h2 className="card-title"> {this.state.thisEvent.title}</h2>
-                    <span className="card-location" > <i className="fa fa-globe" aria-hidden="true"> </i>{this.state.thisEvent.city}-{this.state.thisEvent.country}</span> <hr />
+                    <span className="card-location" > <i className="fa fa-globe" aria-hidden="true"> </i>{this.state.thisEvent.city}-{this.state.thisEvent.country}</span>
                     <span className="card-description"> <i className="fa fa-plus" aria-hidden="true"> </i>{this.state.thisEvent.description} </span>
 
-                    <h4 className="card-time"> <i className="fa fa-clock-o" aria-hidden="true"></i>{this.state.thisEvent.startTime} / {this.state.thisEvent.endTime} -- <i className="fa fa-calendar" aria-hidden="true"></i>{this.state.thisEvent.eventDate} </h4> 
-                    <h4 className="card-attendees"> Max number of people: {this.state.thisEvent.attendees} <button className="btn-join">Join the Event</button></h4>
-                    
-
+                    <h4 className="card-time"> <i className="fa fa-clock-o" aria-hidden="true"></i>{this.state.thisEvent.startTime} / {this.state.thisEvent.endTime} -- <i className="fa fa-calendar" aria-hidden="true"></i>{this.state.thisEvent.eventDate} </h4>
+                    <h4 className="card-attendees">Max number of people: {this.state.thisEvent.attendees} <button className="btn-join">Join the Event</button></h4>
+                
                 </div>
-            </div>
+                <div className="map">
+                    <MapContainer />
+                </div>
+
+            </div >
         )
     }
 
 }
+
 export default DetailsEvent;
